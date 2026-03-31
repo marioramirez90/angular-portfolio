5ac350282f8295e91321242adada8f0d061d2e2e
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-31 14:22:18 +0200

    add Portfolio, References, and Contact components to landing page

--- src/app/components/landingpage/landingpage.ts
+++ src/app/components/landingpage/landingpage.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
 import { AboutMe } from "./about-me/about-me";
 import { MySkills } from "./my-skills/my-skills";
+import { Portfolio } from "./portfolio/portfolio";
+import { References } from "./references/references";
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-landingpage',
-  imports: [Hero, AboutMe, MySkills],
+  imports: [Hero, AboutMe, MySkills, Portfolio, References, Contact],
   templateUrl: './landingpage.html',
   styleUrl: './landingpage.scss',
 })
