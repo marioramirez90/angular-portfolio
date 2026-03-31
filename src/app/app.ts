2e79ba497ebfba03e2bacaf75be32b5addd431ec
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-31 12:50:24 +0200

    add contact and references components with tests; update app structure to include header and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Landingpage } from "./components/landingpage/landingpage";
+import { Header } from "./components/layout/header/header";
+import { Footer } from "./components/layout/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Landingpage],
+  imports: [RouterOutlet, Landingpage, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/landingpage/references/references.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-references',
+  imports: [],
+  templateUrl: './references.html',
+  styleUrl: './references.scss',
+})
+export class References {}
