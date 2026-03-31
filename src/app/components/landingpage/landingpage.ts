c619b667c386997d7400b51054ba7973091db7e2
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-31 12:59:01 +0200

    add MySkills component to landing page

--- src/app/components/landingpage/landingpage.ts
+++ src/app/components/landingpage/landingpage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
 import { AboutMe } from "./about-me/about-me";
+import { MySkills } from "./my-skills/my-skills";
 
 @Component({
   selector: 'app-landingpage',
-  imports: [Hero, AboutMe],
+  imports: [Hero, AboutMe, MySkills],
   templateUrl: './landingpage.html',
   styleUrl: './landingpage.scss',
 })
