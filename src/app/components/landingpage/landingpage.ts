3ccc0aeeb7a1757ac04116ee6f903b48892f2f66
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-31 12:37:10 +0200

    import landingpage to app

--- src/app/components/landingpage/landingpage.ts
+++ src/app/components/landingpage/landingpage.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Hero } from "./hero/hero";
 
 @Component({
   selector: 'app-landingpage',
-  imports: [],
+  imports: [Hero],
   templateUrl: './landingpage.html',
   styleUrl: './landingpage.scss',
 })
