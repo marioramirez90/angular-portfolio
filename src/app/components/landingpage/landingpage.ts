0c4e815251272c9099a598dee06faead49545e57
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-31 12:17:31 +0200

    add components/landingpage

---
+++ src/app/components/landingpage/landingpage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [],
+  templateUrl: './landingpage.html',
+  styleUrl: './landingpage.scss',
+})
+export class Landingpage {}
