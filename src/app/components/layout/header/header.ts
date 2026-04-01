8b39515bbb913bf419f3f1460bba208c13f45e39
marioramirez90 <mario-ramirez@hotmail.de>
2026-04-01 12:58:23 +0200

    refactor header component; enhance language button functionality and styles, update navigation links

--- src/app/components/layout/header/header.ts
+++ src/app/components/layout/header/header.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+    currentLang: string = 'DE';
+
+  setLanguage(lang: string) {
+    this.currentLang = lang;
+  
+  }
+
+  
+}
+
