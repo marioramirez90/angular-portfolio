ccd77c5b0501a23c6265baac3a831951101ae6f0
marioramirez90 <mario-ramirez@hotmail.de>
2026-04-10 13:31:48 +0200

    Add references section with dynamic content and styling

--- src/app/components/landingpage/references/references.ts
+++ src/app/components/landingpage/references/references.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './references.html',
   styleUrl: './references.scss',
 })
-export class References {}
+export class References {
+
+references = [
+{
+name: 'V.Schuster',
+discripton: 'Mario really kept the team together with his great organization and clear communication. We wouldn t have got this far without his commitment.',
+img: 'img/references/1.png'
+},
+{
+name: 'E.Eichinger',
+discripton: 'Michael really kept the team together with his great organization and clear communication. We wouldn t have got this far without his commitment.',
+img: 'img/references/2.png'
+},
+{
+name: 'I.Nuber',
+discripton: 'Mario really kept the team together with his great organization and clear communication. We wouldn t have got this far without his commitment.',
+img: 'img/references/3.png'
+}
+];
+
+currentIndex = 0;
+
+next() {
+this.currentIndex =
+(this.currentIndex + 1) % this.references.length;
+}
+
+prev() {
+this.currentIndex =
+(this.currentIndex - 1 + this.references.length) % this.references.length;
+}
+
+}
