addc549b2dd33ff877c4e4c3b614f19053898330
marioramirez90 <mario-ramirez@hotmail.de>
2026-04-13 18:08:33 +0200

    Add contact form with validation: implement user input fields for name, email, and message with validation rules

--- src/app/components/landingpage/contact/contact.ts
+++ src/app/components/landingpage/contact/contact.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+
+ userform = new FormGroup({
+    name: new FormControl('', {
+      validators: [Validators.required,Validators.minLength(4)]
+    }),
+
+    email: new FormControl('', {
+      validators: [Validators.required,Validators.email]
+
+    }),
+    text: new FormControl('', {
+      validators: [Validators.required,Validators.minLength(10)]
+
+    }),
+  })
+  
+
+}
