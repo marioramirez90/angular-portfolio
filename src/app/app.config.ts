57b3ff6f5e6cc2a85c2f9e35316568a840df8965
marioramirez90 <mario-ramirez@hotmail.de>
2026-06-04 18:57:02 +0200

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideTranslateService({
-      defaultLanguage: 'en',
+      fallbackLang: 'en',
     }),
     provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
   ]
