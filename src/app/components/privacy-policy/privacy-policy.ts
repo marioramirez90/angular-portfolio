ff94a52b4799163ceadaefbfd8195a138297cc64
marioramirez90 <mario-ramirez@hotmail.de>
2026-06-05 11:14:39 +0200

    feat: add privacy-policy and legal-notice components with responsive styling and translation support

--- src/app/components/privacy-policy/privacy-policy.ts
+++ src/app/components/privacy-policy/privacy-policy.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [TranslatePipe],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
